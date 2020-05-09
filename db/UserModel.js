843c0de74545666d2777361fd78100912c0ef661
Eric <375770336@qq.com>
2020-05-10 01:55:20 +0800

    用户

--- db/UserModel.js
+++ db/UserModel.js
@@ -4,24 +4,62 @@ const Sequelize = require('sequelize')
 const Model = Sequelize.Model;
 
 
-const User = sequelize.define('user', {
+class User extends Model { }
+User.init({
     // attributes
     username: {
         type: Sequelize.STRING,
+        unique: true,
         allowNull: false
     },
     password: {
         type: Sequelize.STRING,
         allowNull: false
+    },
+    user_type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 0
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
     }
 }, {
+    sequelize,
+    modelName: 'user'
     // options
 });
+
+// User.sync({ force: true }).then(() => {
+//     // 现在 `users` 表会与模型定义一致
+//     return User.create({
+//         username: 'admin',
+//         password: '123456',
+//         user_type: 0,
+//         email: '375770336@qq.com'
+//     });
+// });
+
+// for (let index = 0; index < 50; index++) {
+//     User.sync({ force: true }).then(() => {
+//     // 现在 `users` 表会与模型定义一致
+//     return User.create({
+//         username: `admin${index}`,
+//         password: `123456${index}`,
+//         user_type: 0,
+//         email: `375770336${index}@qq.com`
+//     });
+// });
+// }
+
+
 
 // sequelize.sync().then(function () {
 //     return User.create({
 //         username: 'wang',
-//         password: '123'
+//         password: '123',
+//         user_type: 0
 //     });
 // }).then(function (data) {
 //     //获取数据
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const jwt = require("jsonwebtoken");
+const JWT = require('jsonwebtoken');
 
 const User = require('../db/UserModel')
-const { createToken, checkToken, SECRET } = require('../util/jwt')
+const { createToken, checkToken, SECRET } = require('../util/JWT')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -38,13 +38,50 @@ router.post('/login', async (req, res) => {
   })
 })
 
-router.post('/profile', async (req, res) => {
-  const raw = req.headers.authorization
-  const { id } = jwt.verify(raw, SECRET)
-  const users = await User.findByPk(id)
-  res.send(users)
-});
+// 获取用户列表
+router.post('/list', async (req, res, next) => {
+  console.log(req.body.params.pagenum, 'body');
 
+  const token = req.headers.authorization
+  JWT.verify(token, SECRET, async (err, decoded) => {
+    if (err) {
+      //验证失败
+      //console.log("令牌失效");
+      res.status(403).send({
+        'meta': {
+          'msg': '登录失败',
+          'status': 403
+        }
+      })
+    } else {
+      //验证成功
+      //获取前端需要的相应数据
+      //返回给前端相应的信息
+
+      const usersList = await User.findAndCountAll({
+        offset: req.body.params.pagesize,
+        limit: req.body.params.pagenum,
+      })
+
+      res.send({
+        'data': {
+          'users': usersList
+        },
+        'meta': {
+          'msg': '获取用户列表成功',
+          'status': 200
+        }
+      })
+    }
+  })
+})
+
+router.post('/profile', async (req, res) => {
+  const token = req.headers.authorization
+  const { id } = JWT.verify(token, SECRET)
+  const usersInfo = await User.findByPk(id)
+  res.send(usersInfo)
+});
 
 router.get('/menus', function (req, res, next) {
   res.send({
@@ -65,12 +102,12 @@ router.get('/menus', function (req, res, next) {
       {
         authName: '权限管理',
         id: 103,
-        path: '"rights',
+        path: 'rights',
         children: [{
           authName: '角色列表',
           children: '',
           id: 111,
-          path: '"roles"'
+          path: 'roles'
         }]
       }
     ],
